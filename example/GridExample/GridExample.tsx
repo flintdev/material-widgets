725d2b63ac28cc506ce353767282ba2e09378dc4
cc189 <cc189dev@gmail.com>
2020-02-24 22:46:16 -0800

    publish v0.0.7

--- example/GridExample/GridExample.tsx
+++ example/GridExample/GridExample.tsx
@@ -7,15 +7,62 @@ export default class GridExample extends React.Component<any, any> {
 
     render() {
         return (
-            <div style={{display: `flex`}}>
+            <div style={{display: `flex`, flexDirection: `column`, height: `100vh`}}>
                 {getWidget(WidgetName.Grid, {
                     params: {
-                        style: {
-                            width: '50vw',
-                            height: '50vw',
-                            display: 'flex'
-                        }
+                        container: true
                     },
+                    columnParams: [
+                        {
+                            style: {
+                                backgroundColor: 'red'
+                            },
+                            xs: 3
+                        },
+                        {
+                            style: {
+                                backgroundColor: 'yellow'
+                            },
+                            xs: 3
+                        },
+                        {
+                            style: {
+                                backgroundColor: 'green'
+                            },
+                            xs: 3
+                        },
+                        {
+                            style: {
+                                backgroundColor: 'blue'
+                            },
+                            xs: 3
+                        },
+                    ],
+                    children: [
+                        getWidget(WidgetName.TextField, {
+                            params: {
+                                style: {
+                                    backgroundColor: 'lightyellow',
+                                    width: '100px'
+                                },
+                                label: 'input1',
+                                variant: "outlined"
+                            },
+                            tag: 'container2',
+                            dnd: true,
+                            draggableProps: {
+                                draggableId: 'drag1',
+                                index: 1    
+                            },
+                            draggableRootStyle: (isDragging: boolean) => {
+                                return {
+                                    cursor: 'move',
+                                    borderRight: `5px solid ${isDragging ? 'red' : 'darkred'}`,
+                                    display: 'inline-table'
+                                }
+                            }
+                        } as WidgetProps)
+                    ],
                     dnd: true,
                     draggableProps: {
                         draggableId: 'drag0',
@@ -25,38 +72,16 @@ export default class GridExample extends React.Component<any, any> {
                         return {
                             cursor: 'move',
                             borderRight: `5px solid ${isDragging ? 'red' : 'darkred'}`,
-                            display: 'inline-table'
+                            height: '100%',
+                            width: '100%',
+                            display: `flex`
                         }
                     },
                     droppableContainerStyle: (isDraggingOver: boolean) => {
                         return {
-                            backgroundColor: isDraggingOver ? 'grey' : 'lightgrey',
+                            backgroundColor: isDraggingOver ? 'grey' : '#eeeeee80',
                             width: '100%',
-                            height: '100%',
-                            marginLeft: 10
-                        }
-                    }
-                } as WidgetProps)}
-
-                {getWidget(WidgetName.TextField, {
-                    params: {
-                        style: {
-                            backgroundColor: 'lightyellow',
-                            width: '100px'
-                        },
-                        label: 'input1',
-                        variant: "outlined"
-                    },
-                    dnd: true,
-                    draggableProps: {
-                        draggableId: 'drag1',
-                        index: 1    
-                    },
-                    draggableRootStyle: (isDragging: boolean) => {
-                        return {
-                            cursor: 'move',
-                            borderRight: `5px solid ${isDragging ? 'red' : 'darkred'}`,
-                            display: 'inline-table'
+                            height: '100%'
                         }
                     }
                 } as WidgetProps)}
--- src/control/Grid/Grid.tsx
+++ src/control/Grid/Grid.tsx
@@ -5,18 +5,20 @@ import { default as MuiGrid, GridProps } from "@material-ui/core/Grid";
 import { Widget, WidgetProps } from "@flintdev/widget-builder";
 
 export interface Props extends WidgetProps {
-    params: GridProps
+    params: GridProps,
+    columnParams?: Array<GridProps>
 }
 
 export default class Grid extends Widget<Props> {
 
     renderCustomComponent() {
-        const { params } = this.props;
+        const { params, columnParams } = this.props;
         return (
             <MuiGrid {...params}>
-                {this.placeContainer('container1')}
-                {this.placeContainer('container2')}
-                {this.placeContainer('container3')}
+                {!!columnParams && columnParams.map((columnParam: GridProps, index: number) => {
+                    console.log(columnParam)
+                    return <MuiGrid {...columnParam}>{this.placeContainer(`container${index}`)}</MuiGrid>
+                })}
             </MuiGrid>
         )
     }
